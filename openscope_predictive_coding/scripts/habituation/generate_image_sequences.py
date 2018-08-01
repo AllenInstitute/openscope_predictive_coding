e5b325c92373e8f10b232d9cd007628055c315a4
nicholasc <nicain.seattle@gmail.com>
2018-08-01 16:32:31 -0700

    build checksum file as part of data generation script

--- openscope_predictive_coding/scripts/habituation/generate_image_sequences.py
+++ openscope_predictive_coding/scripts/habituation/generate_image_sequences.py
@@ -6,9 +6,10 @@ import openscope_predictive_coding as opc
 from openscope_predictive_coding import utilities
 import random
 import hashlib
+import json
 
 src_file_name = '/allen/aibs/technology/nicholasc/openscope/NATURAL_SCENES_LUMINANCE_MATCHED.npy'
-tgt_dir = '/allen/aibs/technology/nicholasc/openscope'
+tgt_dir = '//allen/aibs/technology/nicholasc/openscope'
 
 src_image_data = np.load(src_file_name)
 
@@ -25,14 +26,24 @@ def generate_sequence_block(base_seq, save_file_name=None):
         data_block[ii,:,:] = src_image_data[idx,:,:]
 
     np.save(save_file_name, data_block)
-    return hashlib.md5(data_block).hexdigest()
+    return hashlib.md5(data_block).hexdigest(), save_file_name
+
+habituation_pilot_checksum_dict = {}
 
 # Generate habituated sequence block: 
-habituated_sequence_block_checksum = generate_sequence_block(opc.HABITUATED_SEQUENCE_IMAGES)
+habituated_sequence_block_checksum, habituated_sequence_block_file_name = generate_sequence_block(opc.HABITUATED_SEQUENCE_IMAGES)
 assert habituated_sequence_block_checksum == 'd1de3dfc46a972d323c7b52b9fff78a9'
+habituation_pilot_checksum_dict[habituated_sequence_block_file_name] = habituated_sequence_block_checksum
 
 # Generate randomized oddballs block:
 habituation_oddball_full_sequence = utilities.get_shuffled_repeated_sequence(opc.RANDOMIZED_ODDBALL_UNEXPECTED_IMAGES, 100, seed=0)
-randomized_oddballs_block_checksum = generate_sequence_block(habituation_oddball_full_sequence, save_file_name=os.path.join(tgt_dir, 'habituation_randomized_oddball.npy'))
+randomized_oddballs_block_checksum, randomized_oddballs_file_name = generate_sequence_block(habituation_oddball_full_sequence, save_file_name=os.path.join(tgt_dir, 'habituation_randomized_oddball.npy'))
 assert randomized_oddballs_block_checksum == 'c38555394253b83f42e2a257e1830c20'
+habituation_pilot_checksum_dict[randomized_oddballs_file_name] = randomized_oddballs_block_checksum
+
+# Also add checksum for natural movie 1 from Brain Observatory:
+habituation_pilot_checksum_dict[os.path.join(tgt_dir, 'NATURAL_MOVIE_ONE.npy')] = 'b174ad09736c870c6915baf82cf2c9ad'
+
+# Dump to pilot data directory:
+json.dump(habituation_pilot_checksum_dict, open(os.path.join(tgt_dir, 'habituation_pilot_checksum.json'), 'w'))
 
