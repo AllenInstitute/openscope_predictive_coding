6bf91be172f1d3ae935813e656cddb4e6491e7eb
matchings <marinag@alleninstitute.org>
2018-09-15 15:40:51 -0700

    create response_df for sweeps in ResponseAnalysis

--- openscope_predictive_coding/ophys/io/create_analysis_files.py
+++ openscope_predictive_coding/ophys/io/create_analysis_files.py
@@ -1,42 +1,18 @@
-from visual_behavior.ophys.dataset.visual_behavior_ophys_dataset import VisualBehaviorOphysDataset
-from visual_behavior.ophys.response_analysis.response_analysis import ResponseAnalysis
-import visual_behavior.ophys.response_analysis.utilities as ut
+from openscope_predictive_coding.ophys.dataset.openscope_predictive_coding_dataset import OpenScopePredictiveCodingDataset
+from openscope_predictive_coding.ophys.response_analysis.response_analysis import ResponseAnalysis
 
 import matplotlib
 import logging
 
 matplotlib.use('Agg')
 
-logger = logging.getLogger(__name__)
-
 
 def create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True):
-    logger.info(experiment_id)
-    logger.info('saving ', str(experiment_id), 'to', cache_dir)
-    dataset = VisualBehaviorOphysDataset(experiment_id, cache_dir)
+    print('saving ', str(experiment_id), 'to', cache_dir)
+    dataset = OpenScopePredictiveCodingDataset(experiment_id, cache_dir)
     analysis = ResponseAnalysis(dataset, overwrite_analysis_files)
 
-    logger.info('plotting experiment summary figure')
-    from visual_behavior.ophys.plotting import experiment_summary_figures as esf
-    esf.plot_experiment_summary_figure(analysis, save_dir=cache_dir)
-    esf.plot_mean_first_flash_response_by_image_block(analysis, save_dir=cache_dir, ax=None)
 
-    analysis.flash_response_df = ut.annotate_flash_response_df_with_block_set(analysis.flash_response_df)
-    fdf = analysis.flash_response_df.copy()
-    data = ut.add_early_late_block_ratio_for_fdf(fdf)
-    esf.plot_mean_response_across_image_block_sets(data, analysis.dataset.analysis_folder, save_dir=cache_dir, ax=None)
-
-    logger.info('plotting cell responses')
-    from visual_behavior.ophys.plotting import summary_figures as sf
-    for cell in dataset.get_cell_indices():
-        sf.plot_image_response_for_trial_types(analysis, cell, save_dir=analysis.dataset.analysis_dir)
-        sf.plot_image_response_for_trial_types(analysis, cell, save_dir=cache_dir)
-
-        # sf.plot_mean_response_by_repeat(analysis, cell, save_dir=analysis.dataset.analysis_dir)
-        # sf.plot_mean_response_by_image_block(analysis, cell, save_dir=analysis.dataset.analysis_dir)
-        sf.plot_mean_response_by_repeat(analysis, cell, save_dir=cache_dir)
-        sf.plot_mean_response_by_image_block(analysis, cell, save_dir=cache_dir)
-    logger.info('done')
 
 
 if __name__ == '__main__':
@@ -47,13 +23,9 @@ if __name__ == '__main__':
     # # cache_dir = r'/allen/aibs/informatics/swdb2018/visual_behavior'
     # create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
 
-    # import pandas as pd
-    experiment_id = 746270939
-    # manifest = r'\\allen\aibs\informatics\swdb2018\visual_behavior\visual_behavior_data_manifest.csv'
-    cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\visual_behavior_pilot_analysis'
-    create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
+    experiment_id = 746271249
+    # experiment_id = 746270939
+    # experiment_id = 746271665
 
-    # df = pd.read_csv(manifest)
-    # for i, experiment_id in enumerate(df.experiment_id.values):
-    #     print(i, experiment_id)
-    #     create_analysis_files(int(experiment_id), overwrite_analysis_files=True)
+    cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\opc\opc_analysis'
+    create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
--- openscope_predictive_coding/ophys/response_analysis/response_analysis.py
+++ openscope_predictive_coding/ophys/response_analysis/response_analysis.py
@@ -12,18 +12,14 @@ from visual_behavior.ophys.response_analysis import utilities as ut
 
 
 class ResponseAnalysis(object):
-    """ Contains methods for organizing responses by trial or by  visual stimulus flashes in a DataFrame.
+    """ Contains methods for organizing responses by visual stimulus presentation in a DataFrame called 'response_df'.
 
-    For trial responses, a segment of the dF/F trace for each cell is extracted for each trial in the trials records in a +/-4 seconds window (the 'trial_window') around the change time.
+    A segment of the dF/F trace for each cell is extracted for each sweep in the stimulus_table in a +/- x seconds window (the 'sweep_window') around the start_time.
     The mean_response for each cell is taken in a 500ms window after the change time (the 'response_window').
-    The trial_response_df also contains behavioral metadata from the trial records such as lick times, running speed, reward rate, and initial and change stimulus names.
-
-    For stimulus flashes, the mean response is taken in a 500ms window after each stimulus presentation (the 'response_window') in the stimulus_table.
-    The flash_response_df contains the mean response for every cell, for every stimulus flash.
 
     Parameters
     ----------
-    dataset: VisualBehaviorOphysDataset instance
+    dataset: OpenscopePredictiveCodingDataset instance
     overwrite_analysis_files: Boolean, if True will create and overwrite response analysis files.
     This can be used if new functionality is added to the ResponseAnalysis class to modify existing structures or make new ones.
     If False, will load existing analysis files from dataset.analysis_dir, or generate and save them if none exist.
@@ -32,130 +28,74 @@ class ResponseAnalysis(object):
     def __init__(self, dataset, overwrite_analysis_files=False):
         self.dataset = dataset
         self.overwrite_analysis_files = overwrite_analysis_files
-        self.trial_window = [-4, 4]  # time, in seconds, around change time to extract portion of cell trace
-        self.response_window_duration = 0.5  # window, in seconds, over which to take the mean for a given trial or flash
-        self.response_window = [np.abs(self.trial_window[0]), np.abs(self.trial_window[0]) + self.response_window_duration]  # time, in seconds, around change time to take the mean response
+        self.sweep_window = [-1, 2]  # time, in seconds, around start time to extract portion of cell trace
+        self.response_window_duration = 0.5  # window, in seconds, over which to take the mean for a given stimulus sweep
+        self.response_window = [np.abs(self.sweep_window[0]), np.abs(self.sweep_window[0]) + self.response_window_duration]  # time, in seconds, around change time to take the mean response
         self.baseline_window = np.asarray(
             self.response_window) - self.response_window_duration  # time, in seconds, relative to change time to take baseline mean response
-        self.stimulus_duration = self.dataset.task_parameters['stimulus_duration'].values[0]
-        self.blank_duration = self.dataset.task_parameters['blank_duration'].values[0]
+        # self.stimulus_duration = self.dataset.task_parameters['stimulus_duration'].values[0]
+        # self.blank_duration = self.dataset.task_parameters['blank_duration'].values[0]
         self.ophys_frame_rate = self.dataset.metadata['ophys_frame_rate'].values[0]
         self.stimulus_frame_rate = self.dataset.metadata['stimulus_frame_rate'].values[0]
 
-        self.get_trial_response_df()
-        self.get_flash_response_df()
+        self.get_response_df()
 
-    def get_trial_response_df_path(self):
-        path = os.path.join(self.dataset.analysis_dir, 'trial_response_df.h5')
+    def get_response_df_path(self):
+        path = os.path.join(self.dataset.analysis_dir, 'response_df.h5')
         return path
 
-    def generate_trial_response_df(self):
-        print('generating trial response dataframe')
-        running_speed = self.dataset.running_speed.running_speed.values
+    def generate_response_df(self):
+        print('generating response dataframe')
+        # running_speed = self.dataset.running_speed.running_speed.values
         df_list = []
         for cell_index in self.dataset.cell_indices:
-            for trial in self.dataset.trials.trial.values[:-1]:  # ignore last trial to avoid truncated traces
+            for sweep in self.dataset.stimulus_table.sweep.values:  # ignore last trial to avoid truncated traces
                 cell_specimen_id = self.dataset.get_cell_specimen_id_for_cell_index(cell_index)
                 cell_trace = self.dataset.dff_traces[cell_index, :]
-                change_time = self.dataset.trials[self.dataset.trials.trial == trial].change_time.values[0]
+                start_time = self.dataset.stimulus_table[self.dataset.stimulus_table.sweep == sweep].start_time.values[0]
 
-                trace, timestamps = ut.get_trace_around_timepoint(change_time, cell_trace,
+                trace, timestamps = ut.get_trace_around_timepoint(start_time, cell_trace,
                                                                   self.dataset.timestamps_ophys,
-                                                                  self.trial_window, self.ophys_frame_rate)
+                                                                  self.sweep_window, self.ophys_frame_rate)
                 mean_response = ut.get_mean_in_window(trace, self.response_window, self.ophys_frame_rate)
                 baseline_response = ut.get_mean_in_window(trace, self.baseline_window, self.ophys_frame_rate)
                 p_value = ut.get_p_val(trace, self.response_window, self.ophys_frame_rate)
                 sd_over_baseline = ut.get_sd_over_baseline(trace, self.response_window, self.baseline_window,
                                                            self.ophys_frame_rate)
 
-                # this is redundant because its the same for every cell. do we want to keep this?
-                running_speed_trace, running_speed_timestamps = ut.get_trace_around_timepoint(change_time,
-                                                                                              running_speed,
-                                                                                              self.dataset.timestamps_stimulus,
-                                                                                              self.trial_window,
-                                                                                              self.stimulus_frame_rate)
-                mean_running_speed = ut.get_mean_in_window(running_speed_trace, self.response_window,
-                                                           self.stimulus_frame_rate)
+                # # this is redundant because its the same for every cell. do we want to keep this?
+                # running_speed_trace, running_speed_timestamps = ut.get_trace_around_timepoint(change_time,
+                #                                                                               running_speed,
+                #                                                                               self.dataset.timestamps_stimulus,
+                #                                                                               self.sweep_window,
+                #                                                                               self.stimulus_frame_rate)
+                # mean_running_speed = ut.get_mean_in_window(running_speed_trace, self.response_window,
+                #                                            self.stimulus_frame_rate)
 
                 df_list.append(
-                    [trial, cell_index, cell_specimen_id, trace, timestamps, mean_response, baseline_response,
-                     p_value, sd_over_baseline, running_speed_trace, running_speed_timestamps,
-                     mean_running_speed])
+                    [sweep, cell_index, cell_specimen_id, trace, timestamps, mean_response, baseline_response,
+                     p_value, sd_over_baseline]) #, running_speed_trace, running_speed_timestamps, mean_running_speed])
 
-        columns = ['trial', 'cell', 'cell_specimen_id', 'trace', 'timestamps', 'mean_response', 'baseline_response',
-                   'p_value', 'sd_over_baseline', 'running_speed_trace', 'running_speed_timestamps',
-                   'mean_running_speed']
-        trial_response_df = pd.DataFrame(df_list, columns=columns)
-        trial_metadata = self.dataset.trials
-        trial_metadata = trial_metadata.rename(columns={'response': 'behavioral_response'})
-        trial_metadata = trial_metadata.rename(columns={'response_type': 'behavioral_response_type'})
-        trial_metadata = trial_metadata.rename(columns={'response_time': 'behavioral_response_time'})
-        trial_metadata = trial_metadata.rename(columns={'response_latency': 'behavioral_response_latency'})
-        trial_response_df = trial_response_df.merge(trial_metadata, on='trial')
-        return trial_response_df
+        columns = ['sweep', 'cell_index', 'cell_specimen_id', 'trace', 'timestamps', 'mean_response', 'baseline_response',
+                   'p_value', 'sd_over_baseline'] #, 'running_speed_trace', 'running_speed_timestamps', 'mean_running_speed']
+        response_df = pd.DataFrame(df_list, columns=columns)
+        response_df = response_df.merge(stimulus_table, on='sweep')
+        return response_df
 
-    def save_trial_response_df(self, trial_response_df):
-        print('saving trial response dataframe')
-        trial_response_df.to_hdf(self.get_trial_response_df_path(), key='df', format='fixed')
+    def save_response_df(self, response_df):
+        print('saving response dataframe')
+        response_df.to_hdf(self.get_response_df_path(), key='df', format='fixed')
 
-    def get_trial_response_df(self):
+    def get_response_df(self):
         if self.overwrite_analysis_files:
             print('overwriting analysis files')
-            self.trial_response_df = self.generate_trial_response_df()
-            self.save_trial_response_df(self.trial_response_df)
+            self.response_df = self.generate_response_df()
+            self.save_response_df(self.response_df)
         else:
-            if os.path.exists(self.get_trial_response_df_path()):
-                print('loading trial response dataframe')
-                self.trial_response_df = pd.read_hdf(self.get_trial_response_df_path(), key='df', format='fixed')
+            if os.path.exists(self.get_response_df_path()):
+                print('loading response dataframe')
+                self.response_df = pd.read_hdf(self.get_response_df_path(), key='df', format='fixed')
             else:
-                self.trial_response_df = self.generate_trial_response_df()
-                self.save_trial_response_df(self.trial_response_df)
-        return self.trial_response_df
-
-    def get_flash_response_df_path(self):
-        path = os.path.join(self.dataset.analysis_dir, 'flash_response_df.h5')
-        return path
-
-    def generate_flash_response_df(self):
-        row = []
-        for cell in range(self.dataset.dff_traces.shape[0]):
-            cell_specimen_id = self.dataset.get_cell_specimen_id_for_cell_index(cell)
-            for flash in self.dataset.stimulus_table.flash_number:
-                flash_data = self.dataset.stimulus_table[self.dataset.stimulus_table.flash_number == flash]
-                flash_time = flash_data.start_time.values[0]
-                image_name = flash_data.image_name.values[0]
-                trace_window = [-self.response_window_duration, self.response_window_duration]
-                trace, timestamps = ut.get_trace_around_timepoint(flash_time, self.dataset.dff_traces[cell],
-                                                                  self.dataset.timestamps_ophys,
-                                                                  trace_window, self.ophys_frame_rate)
-                response_window = [self.response_window_duration, self.response_window_duration * 2]
-                p_value = ut.get_p_val(trace, response_window, self.ophys_frame_rate)
-                mean_response = ut.get_mean_in_window(trace, response_window, self.ophys_frame_rate)
-
-                row.append([cell, cell_specimen_id, flash, flash_time, image_name, mean_response, p_value])
-        flash_response_df = pd.DataFrame(data=row,
-                                         columns=['cell', 'cell_specimen_id','flash_number', 'start_time', 'image_name', 'mean_response',
-                                                  'p_value'])
-
-        stimulus_table = self.dataset.stimulus_table.copy()
-        flash_response_df = ut.annotate_flash_response_df_with_pref_stim(flash_response_df)
-        flash_response_df = ut.add_repeat_number_to_flash_response_df(flash_response_df, stimulus_table)
-        flash_response_df = ut.add_image_block_to_flash_response_df(flash_response_df, stimulus_table)
-        return flash_response_df
-
-    def save_flash_response_df(self, flash_response_df):
-        print('saving flash response dataframe')
-        flash_response_df.to_hdf(self.get_flash_response_df_path(), key='df', format='fixed')
-
-    def get_flash_response_df(self):
-        if self.overwrite_analysis_files:
-            self.flash_response_df = self.generate_flash_response_df()
-            self.save_flash_response_df(self.flash_response_df)
-        else:
-            if os.path.exists(self.get_flash_response_df_path()):
-                print('loading flash response dataframe')
-                self.flash_response_df = pd.read_hdf(self.get_flash_response_df_path(), key='df', format='fixed')
-            else:
-                self.flash_response_df = self.generate_flash_response_df()
-                self.save_flash_response_df(self.flash_response_df)
-        return self.flash_response_df
+                self.response_df = self.generate_response_df()
+                self.save_response_df(self.response_df)
+        return self.response_df
