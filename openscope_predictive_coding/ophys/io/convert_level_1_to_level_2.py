7452d14412061faffe48cb1102d976ac8bd05337
matchings <marinag@alleninstitute.org>
2018-10-06 18:12:28 -0700

    linux filepaths in conversion

--- openscope_predictive_coding/ophys/io/convert_level_1_to_level_2.py
+++ openscope_predictive_coding/ophys/io/convert_level_1_to_level_2.py
@@ -624,7 +624,7 @@ if __name__ == '__main__':
     import sys
 
     experiment_id = sys.argv[1]
-    cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\opc\opc_analysis'
+    cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/opc/opc_analysis'
     ophys_data = convert_level_1_to_level_2(experiment_id, cache_dir)
 
     # lims_ids = [746270939, 746271249, 750534428, 752473496, 755645715,
