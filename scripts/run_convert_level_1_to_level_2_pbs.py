18dd4e064296d417f31e112fe810ef845534ed3f
matchings <marinag@alleninstitute.org>
2019-01-10 01:41:43 -0800

    add experiments to cluster scripts

--- scripts/run_convert_level_1_to_level_2_pbs.py
+++ scripts/run_convert_level_1_to_level_2_pbs.py
@@ -4,7 +4,14 @@ if platform.system() == 'Linux':
     sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/Doug/pbstools')
 from pbstools import PythonJob # flake8: noqa: E999
 
-lims_ids =
+# lims_ids = [746270939, 746271249, 750534428, 752473496,
+#              755645715, 754579284, 755000515, 755646041,
+#              756118440, 746271665, 750845430, 750846019,
+#              752473630, 755645219, 756118288, 758305436,
+#              759037671, # pilot2
+lims_ids = [768898762, 775613721, 775058863, 776727982, # production
+             770649677, 773454193, 774438447, 773914563]
+
 
 
 python_file = r"/home/marinag/visual_behavior_analysis/scripts/convert_level_1_to_level_2.py"
