b1a1e8fbe1e76920b3641260de9dfc0a87bd2287
matchings <marinag@alleninstitute.org>
2019-01-10 01:35:04 -0800

    copy over scripts files from VBA

---
+++ scripts/run_convert_level_1_to_level_2_pbs.py
@@ -0,0 +1,30 @@
+import sys
+import platform
+if platform.system() == 'Linux':
+    sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/Doug/pbstools')
+from pbstools import PythonJob # flake8: noqa: E999
+
+lims_ids =
+
+
+python_file = r"/home/marinag/visual_behavior_analysis/scripts/convert_level_1_to_level_2.py"
+
+jobdir = '/allen/programs/braintv/workgroups/nc-ophys/Marina/ClusterJobs/JobRecords_opc'
+
+job_settings = {'queue': 'braintv',
+                'mem': '10g',
+                'walltime': '5:00:00',
+                'ppn': 1,
+                'jobdir': jobdir,
+                }
+
+for lims_id in lims_ids:
+    print(lims_id)
+    PythonJob(
+        python_file,
+        python_executable='/home/marinag/anaconda2/envs/visual_behavior_ophys/bin/python',
+        python_args=lims_id,
+        conda_env=None,
+        jobname='process_{}'.format(lims_id),
+        **job_settings
+    ).run(dryrun=False)
