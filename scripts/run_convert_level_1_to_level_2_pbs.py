f6ec565742aa07da5bf4234e30b33f708e659c4f
matchings <marinag@alleninstitute.org>
2019-07-11 17:59:06 -0700

    use different environment

--- scripts/run_convert_level_1_to_level_2_pbs.py
+++ scripts/run_convert_level_1_to_level_2_pbs.py
@@ -27,7 +27,7 @@ for experiment_id in experiment_ids:
     print(experiment_id)
     PythonJob(
         python_file,
-        python_executable='/home/marinag/anaconda2/envs/visual_behavior_ophys/bin/python',
+        python_executable='/home/marinag/anaconda2/envs/openscope_predictive_coding/bin/python',
         python_args=int(experiment_id),
         conda_env=None,
         jobname='process_{}'.format(int(experiment_id)),
