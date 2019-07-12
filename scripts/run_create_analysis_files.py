59fb9e87b57b298a56b82bacb22945b5ddaea1be
matchings <marinag@alleninstitute.org>
2019-07-11 19:10:24 -0700

    change manifest name

--- scripts/run_create_analysis_files.py
+++ scripts/run_create_analysis_files.py
@@ -8,7 +8,7 @@ from pbstools import PythonJob # flake8: noqa: E999
 
 
 cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/opc/opc_analysis'
-manifest = pd.read_excel(os.path.join(cache_dir, 'opc_manifest.xlsx'))
+manifest = pd.read_excel(os.path.join(cache_dir, 'opc_production_manifest.xlsx'))
 experiment_ids = manifest['experiment_id'].values
 
 python_file = r"/home/marinag/openscope_predictive_coding/scripts/create_analysis_files.py"
